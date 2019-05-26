f0d715350ba9421d5be9c59be614b27f753a45ee
Ken Sugiura <ken.sugiura@gmail.com>
2019-05-26 05:52:59 -0400

    Implement withUniqueId HOC using React Hooks

--- src/assets/js/app/hocs/withUniqueId.js
+++ src/assets/js/app/hocs/withUniqueId.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 import uniqueId from '../utils/uniqueId';
 
+function useUniqueId(propId) {
+    const [id, setId] = useState(propId);
+
+    if (!id) {
+        const uid = uniqueId('uid-');
+
+        setId(uid);
+
+        return uid;
+    }
+
+    return id;
+}
+
 export default function withUniqueId(WrappedComponent) {
-    return class extends Component {
-        constructor(props) {
-            super(props);
+    return (props) => {
+        const id = useUniqueId(props.id);
 
-            this.id = props.id || uniqueId('uid-');
-        }
-
-        render() {
-            return (
-                <WrappedComponent {...this.props} id={this.id} />
-            );
-        }
+        return (
+            <WrappedComponent {...props} id={id} />
+        );
     };
 }
