d6316735b154ad052d4de457a7be52cecc5d4828
Ken Sugiura <ken.sugiura@gmail.com>
2019-11-16 03:20:23 -0500

    Remove module option validators

--- src/assets/js/flagwaver/app/modules/WindModule.js
+++ src/assets/js/flagwaver/app/modules/WindModule.js
@@ -1,19 +1,5 @@
-import { isNumeric, isFunction } from '../../utils/TypeUtils';
-import createPropertyValidator from '../../helpers/createPropertyValidator';
 import Wind from '../../subjects/Wind';
-import WindModifiers from '../../subjects/WindModifiers';
 import ControlModule from './ControlModule';
-
-function getModifierFnFromOption(value, defaultValue) {
-    if (isFunction(value)) {
-        return value;
-    } else if (WindModifiers[value]) {
-        return WindModifiers[value];
-    } else {
-        console.error('FlagWaver.WindModule.option: Invalid value.');
-        return null;
-    }
-}
 
 /**
  * @class WindModule
@@ -33,18 +19,6 @@ export default class WindModule extends ControlModule {
     static displayName = 'windModule';
     static Subject = Wind;
 
-    static validate = createPropertyValidator({
-        speed: (value) => {
-            const n = Number(value);
-
-            if (isNumeric(value) && n >= 0) {
-                return n;
-            } else {
-                console.error('FlagWaver.WindModule.option: Invalid value.');
-            }
-        }
-    });
-
     update(deltaTime) {
         this.subject.update(deltaTime);
     }
@@ -52,7 +26,7 @@ export default class WindModule extends ControlModule {
     setOptions(options) {
         this.subject = new this.constructor.Subject(Object.assign(
             this.configOptions,
-            this.constructor.validate(options)
+            options
         ));
     }
 }
